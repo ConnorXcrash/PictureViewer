8ae02855469bdf437d9b3fcb9017e8e6969a3021
Kitt1 <Kitt1@DA-GAMING-PC>
2023-12-14 14:24:50 -0500

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Main = new System.Windows.Forms.Panel();
+            this.Viewer = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.openPicDlg = new System.Windows.Forms.OpenFileDialog();
+            this.Main.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Viewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.Main.Controls.Add(this.Viewer);
+            this.Main.Location = new System.Drawing.Point(12, 12);
+            this.Main.Name = "Main";
+            this.Main.Size = new System.Drawing.Size(970, 514);
+            this.Main.TabIndex = 0;
+            // 
+            // Viewer
+            // 
+            this.Viewer.Location = new System.Drawing.Point(3, 3);
+            this.Viewer.Name = "Viewer";
+            this.Viewer.Size = new System.Drawing.Size(964, 508);
+            this.Viewer.TabIndex = 0;
+            this.Viewer.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(864, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(734, 533);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(602, 533);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Set Background Color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(468, 533);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Open Image";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(415, 537);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(43, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Zoom +";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(369, 537);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(40, 13);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Zoom -";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.LinkColor = System.Drawing.Color.Red;
+            this.linkLabel3.Location = new System.Drawing.Point(311, 537);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(52, 13);
+            this.linkLabel3.TabIndex = 6;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Auto-Size";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 566);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Main);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Picture Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Main.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Viewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Main;
+        private System.Windows.Forms.PictureBox Viewer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.OpenFileDialog openPicDlg;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            this.CenterToScreen(); // Center the form to the screen
+            Viewer.SizeMode = PictureBoxSizeMode.Zoom; // Set the picturebox to zoom
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = "Are you sure you want to exit?";
+            string title = "Exit";
+            MessageBox.Show(message, title, MessageBoxButtons.YesNo);
+            DialogResult result = MessageBox.Show(message, title, MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openPicDlg.FileName = ""; // Set the file name to empty
+            openPicDlg.Filter = "Image Files (*.jpg, *.jpeg, *.png, *.gif, *.bmp) | *.jpg; *.jpeg; *.png; *.gif; *.bmp"; // Set the filter to only show image files 
+
+            openPicDlg.ShowDialog(); // Show the open file dialog
+            if(!string.IsNullOrEmpty(openPicDlg.FileName)) // If the file name is not empty
+            {
+                Viewer.Image = Image.FromFile(openPicDlg.FileName); // Set the image to the file name
+            }
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Viewer.SizeMode = PictureBoxSizeMode.StretchImage; // Set the picturebox to stretch image
+            Viewer.Refresh(); // Refresh the picturebox
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Viewer.SizeMode = PictureBoxSizeMode.Normal; // Set the picturebox to auto size
+
+            Image img = Viewer.Image; // Set the image to the picturebox image
+            Size imgSize = new Size(img.Width + 100, img.Height + 100); // Set the image size to the image width and height
+
+            Bitmap bm = new Bitmap(img, imgSize); // Set the bitmap to the image and image size
+            Graphics g = Graphics.FromImage(bm); // Set the graphics to the bitmap
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic; // Set the interpolation mode to high quality bicubic
+
+            Viewer.Image = bm; // Set the picturebox image to the bitmap
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Viewer.Image = null; // Set the picturebox image to null
+            Viewer.BackColor = Color.Transparent; // Set the picturebox background color to null
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Viewer.SizeMode = PictureBoxSizeMode.Normal; // Set the picturebox to auto size
+
+            Image img = Viewer.Image; // Set the image to the picturebox image
+            Size imgSize = new Size(img.Width - 100, img.Height - 100); // Set the image size to the image width and height
+
+            //Check if new size, width, or height are below 0
+            if (imgSize.Width < 0 || imgSize.Height < 0)
+            {
+                MessageBox.Show("Image size cannot be below 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); // Show error message
+                return; // Return
+            }
+
+            Bitmap bm = new Bitmap(img, imgSize); // Set the bitmap to the image and image size
+            Graphics g = Graphics.FromImage(bm); // Set the graphics to the bitmap
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic; // Set the interpolation mode to high quality bicubic
+
+            Viewer.Image = bm; // Set the picturebox image to the bitmap
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Allows the user to set the color of the picture box
+            ColorDialog colorDlg = new ColorDialog(); // Create a new color dialog
+            colorDlg.AllowFullOpen = true; // Allow the user to open the full color dialog
+            colorDlg.AnyColor = true; // Allow the user to select any color
+            colorDlg.SolidColorOnly = false; // Allow the user to select a solid color
+            colorDlg.ShowDialog();
+
+            Viewer.BackColor = colorDlg.Color; // Set the picturebox background color to the color dialog color
+        }
+    }
+}
